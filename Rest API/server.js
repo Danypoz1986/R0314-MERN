0fce76286493ba63f4256456ebbb4dd0489df855
Daniel Pozzoli <daniel.pozzoli86@gmail.com>
2024-11-08 15:08:49 +0200

    Update mongoose connection and add .gitignore

--- Rest API/server.js
+++ Rest API/server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then (() => console.log('MongoDB connected'))
 .catch ((error) => console.error('MongoDB connection error:', error));
 
