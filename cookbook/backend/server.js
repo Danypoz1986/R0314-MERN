edc2cf75c97577568e5bcbc52ad8afbe9326406c
Daniel Pozzoli <daniel.pozzoli86@gmail.com>
2024-12-10 03:41:36 +0200

    project done

--- cookbook/backend/server.js
+++ cookbook/backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+  .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`)))
   .catch(err => console.log(err));
 
 app.use('/api/recipes', recipeRoutes);
